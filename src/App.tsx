eb75da598db6bb65572b7b92140e760de92287db
Waihon Yew <yewwaihon@gmail.com>
2024-04-07 18:50:23 +0800

    Replace initial app message with initial navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to the fullstack course :-)</h1>
-    </div>
+    <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
+      <div className='container-fluid'>
+        <span className='navbar-brand'>Luv 2 Read</span>
+        <button className='navbar-toggler' type='button'
+          data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown'
+          aria-controls='navbarNavDropdown' aria-expanded='false'
+          aria-label='Toggle Navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id='navbarNavDropdown'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'> Home</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'> Search Books</a>
+            </li>
+
+          </ul>
+          <ul className='navbar-nav ms-auto'>
+            <li className='nav-item m-1'>
+              <a type='button' className='btn btn-outline-light' href='#'>
+                Sign in
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
