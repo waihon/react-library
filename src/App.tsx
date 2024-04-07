64adb3425382f2f72175ba822d846e9e0dae7508
Waihon Yew <yewwaihon@gmail.com>
2024-04-07 21:33:16 +0800

    Create and add Explore Top Books component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
+import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <ExploreTopBooks />
+    </div>
   );
 }
 
