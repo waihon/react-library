0f2570506b3f83de8c0630a125abbc3532cc9aee
Waihon Yew <yewwaihon@gmail.com>
2024-04-10 16:34:51 +0800

    Create and use HomePage component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import './App.css';
-import { Carousel } from './layouts/HomePage/components/Carousel';
-import { ExploreTopBooks } from './layouts/HomePage/components/ExploreTopBooks';
-import { Heros } from './layouts/HomePage/components/Heros';
-import { LibraryServices } from './layouts/HomePage/components/LibraryServices';
+import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
@@ -10,10 +7,7 @@ function App() {
   return (
     <div>
       <Navbar />
-      <ExploreTopBooks />
-      <Carousel />
-      <Heros />
-      <LibraryServices />
+      <HomePage />
       <Footer />
     </div>
   );
