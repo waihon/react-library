23fcdf1f52afb6de540d8eb05553f4b978143840
Waihon Yew <yewwaihon@gmail.com>
2024-04-10 16:30:01 +0800

    Move home page-related components to a subfolder

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
-import { Carousel } from './layouts/HomePage/Carousel';
-import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
-import { Heros } from './layouts/HomePage/Heros';
-import { LibraryServices } from './layouts/HomePage/LibraryServices';
+import { Carousel } from './layouts/HomePage/components/Carousel';
+import { ExploreTopBooks } from './layouts/HomePage/components/ExploreTopBooks';
+import { Heros } from './layouts/HomePage/components/Heros';
+import { LibraryServices } from './layouts/HomePage/components/LibraryServices';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
