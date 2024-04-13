fa46660e19538dcd69c7d8ecf2fb354b208c4a67
Waihon Yew <yewwaihon@gmail.com>
2024-04-13 21:53:08 +0800

    Alternative text for 0 books

--- src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -89,7 +89,7 @@ export const SearchBooksPage = () => {
   }
 
   const indexOfLastBook: number = currentPage * booksPerPage;
-  const indexOfFirstBook: number = indexOfLastBook  - booksPerPage;
+  const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
   let lastItem = booksPerPage * currentPage <= totalAmountOfBooks ?
     booksPerPage * currentPage : totalAmountOfBooks;
 
@@ -148,15 +148,28 @@ export const SearchBooksPage = () => {
               </div>
             </div>
           </div>
-          <div className='mt-3'>
-            <h5>Number of results: ({totalAmountOfBooks})</h5>
-          </div>
-          <p>
-            {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
-          </p>
-          {books.map(book => (
-            <SearchBook book={book} key={book.id} />
-          ))}
+          {totalAmountOfBooks > 0 ?
+            <>
+              <div className='mt-3'>
+                <h5>Number of results: ({totalAmountOfBooks})</h5>
+              </div>
+              <p>
+                {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
+              </p>
+              {books.map(book => (
+                <SearchBook book={book} key={book.id} />
+              ))}
+            </>
+            :
+            <div className='m-5'>
+              <h3>
+                Can't find what you are looking for?
+              </h3>
+              <a type='button' className='btn main-color btn-md px-4 me-md-2 fw-bold text-white' href='#'>
+                Library Services
+              </a>
+            </div>
+          }
           {totalPages > 1 &&
             <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate} />
           }
