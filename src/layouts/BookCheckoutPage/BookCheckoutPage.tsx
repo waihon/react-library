26eb757468248b96bec4b296b0e34c38a6750b37
Waihon Yew <yewwaihon@gmail.com>
2024-04-24 22:55:00 +0800

    Add LatestReviews to BookCheckoutPage

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -4,6 +4,7 @@ import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { StarsReview } from "../Utils/StarsReview";
 import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
 import ReviewModel from "../../models/ReviewModel";
+import { LatestReviews } from "./LatestReviews";
 
 export const BookCheckoutPage = () => {
 
@@ -127,12 +128,13 @@ export const BookCheckoutPage = () => {
               <h2>{book?.title}</h2>
               <h5 className='text-primary'>{book?.author}</h5>
               <p className='lead'>{book?.description}</p>
-              <StarsReview rating={3.5} size={32} />
+              <StarsReview rating={totalStars} size={32} />
             </div>
           </div>
           <CheckoutAndReviewBox book={book} mobile={false} />
         </div>
         <hr />
+        <LatestReviews reviews={reviews} bookId={book?.id} mobile={false} />
       </div>
       {/* Mobile */}
       <div className='container d-lg-none mt-5'>
@@ -149,11 +151,12 @@ export const BookCheckoutPage = () => {
             <h2>{book?.title}</h2>
             <h5 className='text-primary'>{book?.author}</h5>
             <p className='lead'>{book?.description}</p>
-              <StarsReview rating={3.5} size={32} />
+              <StarsReview rating={totalStars} size={32} />
           </div>
         </div>
         <CheckoutAndReviewBox book={book} mobile={true} />
         <hr />
+        <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
       </div>
     </div>
   );
