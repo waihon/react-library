dcc20a6e6abbbfeb40aa9605a89f7b9427fdde01
Waihon Yew <yewwaihon@gmail.com>
2024-04-27 10:06:25 +0800

    Fix the path of oktaConfig

--- src/Auth/OktaSignInWidget.jsx
+++ src/Auth/OktaSignInWidget.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import OktaSignIn from '@okta/okta-signin-widget';
 import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
-import { oktaConfig } from '../../lib/oktaConfig';
+import { oktaConfig } from '../lib/oktaConfig';
 
 const OktaSignInWidget = ({ onSuccess, onError }) => {
   const widgetRef = useRef();
