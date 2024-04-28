e2f893c1a0fe72098f1b322ad6b474a81c880ca8
Waihon Yew <yewwaihon@gmail.com>
2024-04-28 18:40:54 +0800

    Change to https to avoid unsupported response from server

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,6 +1,6 @@
 export const oktaConfig = {
   clientId: '{clientId}',
-  issuer: 'http://{issuer}/oauth2/default',
+  issuer: 'https://{issuer}/oauth2/default',
   redirectUri: 'http://localhost:3000/login/callback',
   scopes: ['openid', 'profile', 'email'],
   pkce: true,
