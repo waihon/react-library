2b3c9408ba9c3774cbe6cd84da8803d2e47b6611
Waihon Yew <yewwaihon@gmail.com>
2024-05-04 11:07:18 +0800

    Add optional Description to LeaveAReview

--- src/layouts/Utils/LeaveAReview.tsx
+++ src/layouts/Utils/LeaveAReview.tsx
@@ -4,9 +4,12 @@ import { StarsReview } from "./StarsReview";
 export const LeaveAReview: React.FC<{}> = (props) => {
 
   const [starInput, setStarInput] = useState(0);
+  const [displayInput, setDisplayInput] = useState(false);
+  const [reviewDescription, setReviewDescription] = useState("");
 
   function starValue(value: number) {
     setStarInput(value);
+    setDisplayInput(true);
   }
 
   return (
@@ -28,6 +31,25 @@ export const LeaveAReview: React.FC<{}> = (props) => {
         <li><button onClick={() => starValue(5.0)} className='dropdown-item'>5 star</button></li>
       </ul>
       <StarsReview rating={starInput} size={32} />
+
+      {displayInput &&
+        <form method='POST' action='#'>
+          <hr />
+
+          <div className='mb-3'>
+            <label className='form-label'>
+              Description
+            </label>
+            <textarea className='form-control' id='submitReviewDescription' placeholder='Optional'
+              rows={3} autoFocus={true} onChange={e => setReviewDescription(e.target.value)}>
+            </textarea>
+          </div>
+
+          <div>
+             <button type='button' className='btn btn-primary mt-3'>Submit Review</button>
+          </div>
+        </form>
+      }
     </div>
   );
 }
