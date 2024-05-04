096920a664b6dcca575fc7135479353470db2fdd
Waihon Yew <yewwaihon@gmail.com>
2024-05-04 17:41:21 +0800

    Review List page HTML/CSS

--- src/layouts/BookCheckoutPage/ReviewListPage/ReviewListPage.tsx
+++ src/layouts/BookCheckoutPage/ReviewListPage/ReviewListPage.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import ReviewModel from "../../../models/ReviewModel";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+import { Review } from "../../Utils/Review";
+import { Pagination } from "../../Utils/Pagination";
 
 export const ReviewListPage = () => {
 
@@ -79,9 +81,22 @@ export const ReviewListPage = () => {
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return (
-    <div>
+    <div className='container m-5'>
+      <div>
+        <h3>Comments: ({reviews.length})</h3>
+      </div>
+      <p>
+        {indexOfFirstReview + 1} to {lastItem} of {totalAmountOfReviews} items:
+      </p>
+      <div className='row'>
+        {reviews.map(review => (
+          <Review review={review} key={review.id} />
+        ))}
+      </div>
 
+      {totalPages > 1 &&
+        <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate} />
+      }
     </div>
   );
-
 }
