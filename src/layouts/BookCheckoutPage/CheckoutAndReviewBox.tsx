0f5465551d55f04f0240bc393bf8d7d0d76858c7
Waihon Yew <yewwaihon@gmail.com>
2024-05-04 10:45:26 +0800

    Create and use LeaveAReview with star input dropdown

--- src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
+++ src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import BookModel from "../../models/BookModel"
+import { LeaveAReview } from "../Utils/LeaveAReview";
 
 export const CheckoutAndReviewBox: React.FC<{ book: BookModel | undefined, mobile: boolean,
   currentLoansCount: number, isAuthenticated: any, isCheckedOut: boolean,
@@ -20,7 +21,7 @@ export const CheckoutAndReviewBox: React.FC<{ book: BookModel | undefined, mobil
 
   function reviewRender() {
     if (props.isAuthenticated && !props.isReviewLeft) {
-      return (<p>Leave a review component here.</p>)
+      return (<LeaveAReview />)
     } else if (props.isAuthenticated && props.isReviewLeft) {
       return (<p><b>Thank you for your review!</b></p>)
     }
