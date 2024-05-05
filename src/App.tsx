682a92bb778a0020e12b425f8b8a058606154fbf
Waihon Yew <yewwaihon@gmail.com>
2024-05-05 09:23:44 +0800

    Define a secure route for Shelf page

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,10 @@ import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 import { oktaConfig } from './lib/oktaConfig';
 import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
-import { Security } from '@okta/okta-react';
+import { SecureRoute, Security } from '@okta/okta-react';
 import LoginWidget from './Auth/LoginWidget';
 import { ReviewListPage } from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
+import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -50,6 +51,9 @@ export const App = () => {
               () => <LoginWidget config={oktaConfig} />
               }
             />
+            <SecureRoute path='/shelf'>
+              <ShelfPage />
+            </SecureRoute>
           </Switch>
         </div>
         <Footer />
