d28bcee50d0ca54ee9a9b524b37662ab9ec6d072
Waihon Yew <yewwaihon@gmail.com>
2024-05-06 23:27:49 +0800

    Add Loans component to Shelf page

--- src/layouts/ShelfPage/ShelfPage.tsx
+++ src/layouts/ShelfPage/ShelfPage.tsx
@@ -1,3 +1,5 @@
+import { Loans } from "./components/Loans";
+
 export const ShelfPage = () => {
   return (
     <div className='container'>
@@ -7,19 +9,19 @@ export const ShelfPage = () => {
             <button className='nav-link active' id='nav-loans-tab' data-bs-toggle='tab'
               data-bs-target='#nav-loans' type='button' role='tab' aria-controls='nav-loans'
               aria-selected='true'>
-                Loans
+              Loans
             </button>
             <button className='nav-link' id='nav-history-tab' data-bs-toggle='tab'
               data-bs-target='#nav-history' type='button' role='tab' aria-controls='nav-history'
               aria-selected='false'>
-                Your History
+              Your History
             </button>
           </div>
         </nav>
         <div className='tab-content' id='nav-tabContent'>
           <div className='tab-pane fade show active' id='nav-loans' role='tabpanel'
             aria-labelledby='nav-loans-tab'>
-            <p>Loans</p>
+            <Loans />
           </div>
           <div className='tab-pane fade' id='nav-history' role='tabpanel'
             aria-labelledby='nav-history-tab'>
