de123c754a89b8c3f8925abc35882f3ab6c7fcb7
Waihon Yew <yewwaihon@gmail.com>
2024-05-06 22:39:39 +0800

    Call currentloans endpoint in Loans component

--- src/layouts/ShelfPage/components/Loans.tsx
+++ src/layouts/ShelfPage/components/Loans.tsx
@@ -1,6 +1,7 @@
 import { useOktaAuth } from "@okta/okta-react";
 import { useEffect, useState } from "react";
 import ShelfCurrentLoans from "../../../models/ShelfCurrentLoans";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 
 export const Loans = () => {
 
@@ -13,7 +14,23 @@ export const Loans = () => {
 
   useEffect(() => {
     const fetchUserCurrentLoans = async () => {
-
+      if (authState && authState.isAuthenticated) {
+        const url = `http://localhost:8080/api/books/secure/currentloans`;
+        const requestOptions = {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+            'Content-Type': 'application/json'
+          }
+        };
+        const shelfCurrentLoansResponse = await fetch(url, requestOptions);
+        if (!shelfCurrentLoansResponse.ok) {
+          throw new Error('Something went wrong!');
+        }
+        const shelfCurrentLoansResponseJson = await shelfCurrentLoansResponse.json();
+        setShelfCurrentLoans(shelfCurrentLoansResponseJson);
+      }
+      setIsLoadingUserLoans(false);
     };
     fetchUserCurrentLoans().catch((error: any) => {
       setIsLoadingUserLoans(false);
@@ -21,6 +38,22 @@ export const Loans = () => {
     });
     window.scrollTo(0, 0);
   }, [authState]);
+
+  if (isLoadingUserLoans) {
+    return (
+      <SpinnerLoading />
+    );
+  }
+
+  if (httpError) {
+    return (
+      <div className='container m-5'>
+        <p>
+          {httpError}
+        </p>
+      </div>
+    )
+  }
 
   return (<></>);
 }
