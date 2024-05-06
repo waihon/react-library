fe963433acfe28ea43dfb47f7c91772ba926c089
Waihon Yew <yewwaihon@gmail.com>
2024-05-06 23:27:00 +0800

    Complete desktop version of HTML/CSS for Loans

--- src/layouts/ShelfPage/components/Loans.tsx
+++ src/layouts/ShelfPage/components/Loans.tsx
@@ -2,6 +2,7 @@ import { useOktaAuth } from "@okta/okta-react";
 import { useEffect, useState } from "react";
 import ShelfCurrentLoans from "../../../models/ShelfCurrentLoans";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+import { Link } from "react-router-dom";
 
 export const Loans = () => {
 
@@ -55,5 +56,81 @@ export const Loans = () => {
     )
   }
 
-  return (<></>);
+  return (
+    <div>
+      {/* Desktop */}
+      <div className='d-none d-lg-block mt-2'>
+        {shelfCurrentLoans.length > 0 ?
+          <>
+            <h5>Current Loans: </h5>
+
+            {shelfCurrentLoans.map(shelfCurrentLoan => (
+              <div key={shelfCurrentLoan.book.id}>
+                <div className='row mt-3 mb-3'>
+                  <div className='col-4 col-md-4 container'>
+                    {shelfCurrentLoan.book?.img ?
+                      <img src={shelfCurrentLoan.book?.img} width='226' height='349' alt='Book' />
+                      :
+                      <img src={require('./../../../Images/BooksImages/book-luv2code-1000.png')}
+                        width='226' height='349' alt='Book' />
+                    }
+                  </div>
+                  <div className='card col-3 col-md-3 container d-flex'>
+                    <div className='card-body'>
+                      <div className='mt-3'>
+                        <h4>Loan Options</h4>
+                        {shelfCurrentLoan.daysLeft > 0 &&
+                          <p className='text-secondary'>
+                            Due in {shelfCurrentLoan.daysLeft} days.
+                          </p>
+                        }
+                        {shelfCurrentLoan.daysLeft === 0 &&
+                          <p className='text-success'>
+                            Due today.
+                          </p>
+                        }
+                        {shelfCurrentLoan.daysLeft < 0 &&
+                          <p className='text-danger'>
+                            Past due by {shelfCurrentLoan.daysLeft} days.
+                          </p>
+                        }
+                        <div className='list-group mt-3'>
+                          <button className='list-group-item list-group-item-action'
+                            aria-current='true' data-bs-toggle='modal'
+                            data-bs-target={`#modal${shelfCurrentLoan.book.id}`}>
+                            Manage Loan
+                          </button>
+                          <Link to={'search'} className='list-group-item list-group-item-action'>
+                            Search more books?
+                          </Link>
+                        </div>
+                      </div>
+                      <hr />
+                      <p className='mt-3'>
+                        Help others find their adventure by reviewing your loan.
+                      </p>
+                      <Link className='btn btn-primary main-color' to={`/checkout/${shelfCurrentLoan.book.id}`}>
+                        Leave a review
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+                <hr />
+              </div>
+            ))}
+          </> :
+          <>
+            <h3 className='mt-3'>
+              Currently no loans
+            </h3>
+            <Link className='btn btn-primary main-color' to={`search`}>
+              Search for a new book
+            </Link>
+          </>
+        }
+      </div>
+
+      {/* Mobile */}
+    </div>
+  );
 }
