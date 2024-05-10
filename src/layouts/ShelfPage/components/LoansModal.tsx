fb7caffd9837aaa8fefe1e121bcfcbd5cf98b1ad
Waihon Yew <yewwaihon@gmail.com>
2024-05-10 23:35:22 +0800

    Implement front-end to Return Book

--- src/layouts/ShelfPage/components/LoansModal.tsx
+++ src/layouts/ShelfPage/components/LoansModal.tsx
@@ -1,6 +1,6 @@
 import ShelfCurrentLoans from "../../../models/ShelfCurrentLoans";
 
-export const LoansModal: React.FC<{ shelfCurrentLoan: ShelfCurrentLoans, mobile: boolean }> = (props) => {
+export const LoansModal: React.FC<{ shelfCurrentLoan: ShelfCurrentLoans, mobile: boolean, returnBook: any }> = (props) => {
   return (
     <div className='modal fade' id={props.mobile ? `mobilemodal${props.shelfCurrentLoan.book.id}` :
       `modal${props.shelfCurrentLoan.book.id}`} data-bs-backdrop='static' data-bs-keyboard='false'
@@ -49,7 +49,7 @@ export const LoansModal: React.FC<{ shelfCurrentLoan: ShelfCurrentLoans, mobile:
                   </p>
                 }
                 <div className='list-group mt-3'>
-                  <button data-bs-dismiss='modal' className='list-group-item list-group-item-action'
+                  <button onClick={() => props.returnBook(props.shelfCurrentLoan.book.id)} data-bs-dismiss='modal' className='list-group-item list-group-item-action'
                     aria-current='true'>
                     Return Book
                   </button>
