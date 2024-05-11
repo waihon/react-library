074172173f4a1aef68f0d26222cae9afb35540ee
Waihon Yew <yewwaihon@gmail.com>
2024-05-11 21:14:00 +0800

    Create secure route for Messages page

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { SecureRoute, Security } from '@okta/okta-react';
 import LoginWidget from './Auth/LoginWidget';
 import { ReviewListPage } from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
 import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
+import { MessagesPage } from './layouts/MessagesPage/MessagesPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -53,6 +54,9 @@ export const App = () => {
             />
             <SecureRoute path='/shelf'>
               <ShelfPage />
+            </SecureRoute>
+            <SecureRoute path='/messages'>
+              <MessagesPage />
             </SecureRoute>
           </Switch>
         </div>
