7aee38d03d7cc2d171b2118841910a6105a3255e
Waihon Yew <yewwaihon@gmail.com>
2024-05-13 20:44:27 +0800

    Click state for Manage Library page

--- src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
+++ src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
@@ -8,25 +8,40 @@ export const ManageLibraryPage = () => {
   const [changeQuantityOfBooksClick, setChangeQuantityOfBooksClick] = useState(false);
   const [messagesClick, setMessagesClick] = useState(false);
 
+  function addBookClickFunction() {
+    setChangeQuantityOfBooksClick(false);
+    setMessagesClick(false);
+  }
+
+  function changeQuantityOfBooksClickFunction() {
+    setChangeQuantityOfBooksClick(true);
+    setMessagesClick(false);
+  }
+
+  function messagesClickFunction() {
+    setChangeQuantityOfBooksClick(false);
+    setMessagesClick(true);
+  }
+
   return(
     <div className='container'>
       <div className='mt-5'>
         <h3>Manage Library</h3>
         <nav>
           <div className='nav nav-tabs' id='nav-tab' role='tablist'>
-            <button className='nav-link active' id='nav-add-book-tab' data-bs-toggle='tab'
+            <button onClick={addBookClickFunction} className='nav-link active' id='nav-add-book-tab' data-bs-toggle='tab'
               data-bs-target='#nav-add-book' type='button' role='tab' aria-controls='nav-add-book'
               aria-selected='false'
             >
               Add new book
             </button>
-            <button className='nav-link' id='nav-add-quantity-tab' data-bs-toggle='tab'
+            <button onClick={changeQuantityOfBooksClickFunction} className='nav-link' id='nav-add-quantity-tab' data-bs-toggle='tab'
               data-bs-target='#nav-quantity' type='button' role='tab' aria-controls='nav-quantity'
               aria-selected='true'
             >
               Change quantity
             </button>
-            <button className='nav-link' id='nav-messages-tab' data-bs-toggle='tab'
+            <button onClick={messagesClickFunction} className='nav-link' id='nav-messages-tab' data-bs-toggle='tab'
               data-bs-target='#nav-messages' type='button' role='tab' aria-controls='nav-messages'
               aria-selected='false'
             >
@@ -43,12 +58,12 @@ export const ManageLibraryPage = () => {
           <div className='tab-pane fade' id='nav-quantity' role='tabpanel'
             aria-labelledby='nav-quantity-tab'
           >
-            Change quantity
+            {changeQuantityOfBooksClick ? <>Change quantity</> : <></>}
           </div>
           <div className='tab-pane fade' id='nav-messages' role='tabpanel'
             aria-labelledby='nav-messages-tab'
           >
-            Admin Messages
+            {messagesClick ? <>Admin Messages</> : <></>}
           </div>
         </div>
       </div>
