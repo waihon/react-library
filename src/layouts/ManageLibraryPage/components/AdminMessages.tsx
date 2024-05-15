8d4c0d4248afb97f0ef5a9bc94479e06718ab620
Waihon Yew <yewwaihon@gmail.com>
2024-05-15 22:48:39 +0800

    Call findByClosed to load open messages

--- src/layouts/ManageLibraryPage/components/AdminMessages.tsx
+++ src/layouts/ManageLibraryPage/components/AdminMessages.tsx
@@ -21,6 +21,25 @@ export const AdminMessages = () => {
 
   useEffect(() => {
     const fetchUserMessages = async () => {
+      if (authState && authState.isAuthenticated) {
+        const url = `http://localhost:8080/api/messages/search/findByClosed?closed=false&page=${currentPage - 1}&size=${messagesPerPage}`;
+        const requestOptions = {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+            'Content-Type': 'application/json'
+          }
+        };
+        const messagesResponse = await fetch(url, requestOptions);
+        if (!messagesResponse.ok) {
+          throw new Error('Something went wrong!');
+        }
+        const messagesResponseJson = await messagesResponse.json();
+
+        setMessages(messagesResponseJson._embedded.messages);
+        setTotalPages(messagesResponseJson.page.totalPages);
+      }
+      setIsLoadingMessages(false);
 
     }
     fetchUserMessages().catch((error: any) => {
