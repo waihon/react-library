6f4d1ebbe325ce970400714a649391249aa39dd2
Waihon Yew <yewwaihon@gmail.com>
2024-05-16 23:52:51 +0800

    Submit response to question

--- src/layouts/ManageLibraryPage/components/AdminMessages.tsx
+++ src/layouts/ManageLibraryPage/components/AdminMessages.tsx
@@ -4,6 +4,7 @@ import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 import { useEffect, useState } from "react";
 import { Pagination } from "../../Utils/Pagination";
 import { AdminMessage } from "./AdminMessage";
+import AdminMessageRequest from "../../../models/AdminMessageRequest";
 
 export const AdminMessages = () => {
 
@@ -20,6 +21,9 @@ export const AdminMessages = () => {
   // Pagination
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
+
+  // Recall useEffect
+  const [btnSubmit, setBtnSubmit] = useState(false);
 
   useEffect(() => {
     const fetchUserMessages = async () => {
@@ -49,7 +53,7 @@ export const AdminMessages = () => {
       setHttpError(error.message);
     })
     window.scrollTo(0, 0)
-  }, [authState, currentPage])
+  }, [authState, currentPage, btnSubmit])
 
   if (isLoadingMessages) {
     return (
@@ -63,6 +67,27 @@ export const AdminMessages = () => {
         <p>{httpError}</p>
       </div>
     );
+  }
+
+  async function submitResponseToQuestion(id: number, response: string) {
+    const url = `http://localhost:8080/api/messages/secure/admin/message`;
+    if (authState && authState?.isAuthenticated && id !== null && response !== '') {
+      const messageAdminRequestModel: AdminMessageRequest = new AdminMessageRequest(id, response);
+      const requestOptions = {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(messageAdminRequestModel)
+      };
+
+      const messageAdminRequestModelResponse = await fetch(url, requestOptions);
+      if (!messageAdminRequestModelResponse.ok) {
+        throw new Error('Something went wrong!');
+      }
+      setBtnSubmit(!btnSubmit);
+    }
   }
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
