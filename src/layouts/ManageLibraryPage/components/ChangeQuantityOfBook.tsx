6655af4ddeb6a2e9440cc5c355e0234d47f5cd95
Waihon Yew <yewwaihon@gmail.com>
2024-05-19 12:48:13 +0800

    HTML/CSS for Change Quantity of Book

--- src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
+++ src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
@@ -15,7 +15,51 @@ export const ChangeQuantityOfBook: React.FC<{ book: BookModel }> = (props, key) 
   }, []);
 
   return (
-    <>
-    </>
+    <div className='card mt-3 shadow p-3 mb-3 bg-body rounded'>
+      <div className='row g-0'>
+        <div className='col-md-2'>
+          {/* Desktop Version */}
+          <div className='d-none d-lg-block'>
+            {props.book.img ?
+              <img src={props.book.img} width='123' height='196' alt='Book' />
+              :
+              <img src={require('./../../../Images/BooksImages/book-luv2code-1000.png')}
+                width='123' height='196' alt='Book' />
+            }
+          </div>
+          {/* Mobile Version */}
+          <div className='d-lg-none d-flex justify-content-center align-items-center'>
+            {props.book.img ?
+              <img src={props.book.img} width='123' height='196' alt='Book' />
+              :
+              <img src={require('./../../../Images/BooksImages/book-luv2code-1000.png')}
+                width='123' height='196' alt='Book' />
+            }
+          </div>
+        </div>
+        <div className='col-md-6'>
+          <div className='card-body'>
+            <h5 className='card-title'>{props.book.author}</h5>
+            <h4>{props.book.title}</h4>
+            <p className='card-text'>{props.book.description}</p>
+          </div>
+        </div>
+        <div className='mt-3 col-md-4'>
+          <div className='d-flex justify-conten-center align-items-center'>
+            <p>Total Quantity: <b>{quantity}</b></p>
+          </div>
+          <div className='d-flex justify-content-center align-items-center'>
+            <p>Books Remaining: <b>{remaining}</b></p>
+          </div>
+        </div>
+        <div className='mt=3 col-md-1'>
+          <div className='d-flex justify-content-start'>
+            <button className='m-1 btn btn-md btn-danger'>Delete</button>
+          </div>
+        </div>
+      </div>
+      <button className='m1 btn btn-md btn-primary main-color text-white'>Add Quantity</button>
+      <button className='m1 btn btn-md btn-warning'>Decrease Quantity</button>
+    </div>
   );
 }
