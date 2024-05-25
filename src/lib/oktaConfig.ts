e69d5af3e1de2333c2ace712cb2c38792e001847
Waihon Yew <yewwaihon@gmail.com>
2024-05-25 22:07:01 +0800

    Okta redirect URL to use HTTPS

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
   clientId: '{clientId}',
   issuer: 'https://{issuer}/oauth2/default',
-  redirectUri: 'http://localhost:3000/login/callback',
+  redirectUri: 'https://localhost:3000/login/callback',
   scopes: ['openid', 'profile', 'email'],
   pkce: true,
   disableHttpsCheck: true,
