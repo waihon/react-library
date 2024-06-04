95b05881bc2c77a61236282231c7e359114df1c0
Waihon Yew <yewwaihon@gmail.com>
2024-06-04 16:15:16 +0800

    Embed App with Stripe Elements

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js'
+
+const stripePromise = loadStripe('pk_test_X5sdcmvl3huJyMnsvMfpQGCr');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <App />
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>
   </BrowserRouter>
 );
